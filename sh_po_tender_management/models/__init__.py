e380da52ef66259eb856ceb07d21d27bf5352f52
Appscom <60970424+Appscom@users.noreply.github.com>
2020-08-14 12:45:41 +0530

    Delete __init__.py

--- sh_po_tender_management/models/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import sh_purchase_agreement_type
-from . import res_users
-from . import sh_purchase_agreement
-from . import sh_purchase_order
-from . import report_analyze_quotation
